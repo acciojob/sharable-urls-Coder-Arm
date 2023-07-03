e8fcc53d34cca5f8ac046009da95bcce993219b7
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-03 11:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // your code here
 let heading = document.getElementById("url");
-const change_heading = () => {
-	heading.innerHTML = window.location.href;
-}
+const button= document.getElementById("button");
+
+button.addEventListener("click",(event) => {
+	event.preventDefault();
+	heading.append("?name="+name.value+"&year="+year.value);
+})
